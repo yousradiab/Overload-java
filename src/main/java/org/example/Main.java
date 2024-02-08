c7f4bdff88e2d077ca3070b31aa3a4b5e1b986f8
yousradiab <yousserah@gmail.com>
2024-02-08 10:01:11 +0100

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,59 @@
 package org.example;
 
+import java.util.HashSet;
+import java.util.Set;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void out(String s) {
+        System.out.println(s);
     }
-}
+
+    public static void out(int i) {
+        System.out.println(i);
+    }
+
+    public static void out(Integer iObj) {
+        System.out.println("Integer =" + iObj);
+
+    }
+
+    public static void out(char c) {
+        System.out.println(c);
+    }
+
+    public static void out(Class aClass) {
+        System.out.println(aClass);
+    }
+
+
+    public static void main(String[] args) {
+        out("hej");
+        out(5);
+        out("hej".indexOf('e'));
+        //parse.Int tager en streng og forsøger at lave om til tal, hvor man angiver hvilket talsystem.
+        out(Integer.parseInt("ff", 16));
+        out(Integer.valueOf("10", 16));
+
+        Integer myInteger = 5;
+        out(myInteger);
+
+// finder ASCII værdien
+        out('a');
+        out("你好");
+
+        for (int i = 36890; i < 36900; i++) {
+            out("Kinsesisk tegn " + (char) +i + i);
+        }
+
+        char h = 'ح';
+        out((int) h);
+        out((char) 1581
+        );
+        var obj ="hej".getClass();
+        out(obj);
+
+    }
+};
